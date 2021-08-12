2b266f1f42f3dae64f3153310559c85190e1f3c6
HerrZYZ <45750637+HerrZYZ@users.noreply.github.com>
2021-08-12 13:25:43 +1000

    Update louvain_core.pyx

--- sknetwork/clustering/louvain_core.pyx
+++ sknetwork/clustering/louvain_core.pyx
@@ -100,16 +100,16 @@ def fit_core(float resolution, float tol, float[:] ou_node_probs, float[:] in_no
                 ratio_in = resolution * node_prob_in
 
                 delta_exit = 2 * (neighbor_clusters_weights[cluster_node] - self_loops[i])
-                delta_exit -= ratio_ou * ( pow((in_clusters_weights[cluster_node] - node_prob_in),3)- pow((1-(in_clusters_weights[cluster_node] - node_prob_in)),3))
-                delta_exit -= ratio_in * ( pow((ou_clusters_weights[cluster_node] - node_prob_ou),3)- pow((1-(ou_clusters_weights[cluster_node] - node_prob_ou)),3))
+                delta_exit -= ratio_ou * ( pow((in_clusters_weights[cluster_node] - node_prob_in),3))- 2 * ratio_ou * pow((1-(in_clusters_weights[cluster_node] - node_prob_in)),3)
+                delta_exit -= ratio_in * ( pow((ou_clusters_weights[cluster_node] - node_prob_ou),3))- 2 * ratio_in * pow((1-(ou_clusters_weights[cluster_node] - node_prob_ou)),3)
                 
                 delta_best = 0
                 cluster_best = cluster_node
 
                 for cluster in unique_clusters:
                     delta = 2 * neighbor_clusters_weights[cluster]
-                    delta -= ratio_ou * ( pow(in_clusters_weights[cluster],3)- pow((1-in_clusters_weights[cluster]),3))
-                    delta -= ratio_in * ( pow(ou_clusters_weights[cluster],3)- pow((1-ou_clusters_weights[cluster]),3))
+                    delta -= ratio_ou * ( pow(in_clusters_weights[cluster],3))- 2 * ratio_ou * pow((1-in_clusters_weights[cluster]),3)
+                    delta -= ratio_in * ( pow(ou_clusters_weights[cluster],3))- 2 * ratio_in * pow((1-ou_clusters_weights[cluster]),3)
 
                     delta_local = delta - delta_exit
                     if delta_local > delta_best:
