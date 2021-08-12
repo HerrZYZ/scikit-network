68f78d0070c7688208feedfa718d182f5a2e7097
HerrZYZ <45750637+HerrZYZ@users.noreply.github.com>
2021-08-12 15:34:50 +1000

    Update louvain_core.pyx

--- sknetwork/clustering/louvain_core.pyx
+++ sknetwork/clustering/louvain_core.pyx
@@ -100,16 +100,34 @@ def fit_core(float resolution, float tol, float[:] ou_node_probs, float[:] in_no
                 ratio_in = resolution * node_prob_in
 
                 delta_exit = 2 * (neighbor_clusters_weights[cluster_node] - self_loops[i])
-                delta_exit -= ratio_ou * ( pow((in_clusters_weights[cluster_node] - node_prob_in),3))- 2 * ratio_ou * pow((1-(in_clusters_weights[cluster_node] - node_prob_in)),3)
-                delta_exit -= ratio_in * ( pow((ou_clusters_weights[cluster_node] - node_prob_ou),3))- 2 * ratio_in * pow((1-(ou_clusters_weights[cluster_node] - node_prob_ou)),3)
-                
+                if (in_clusters_weights[cluster_node] - node_prob_in) >0.15:
+                    delta_exit -= ratio_ou * (10)
+                    delta_exit -= ratio_in * (10)
+                    
+                elif (in_clusters_weights[cluster_node] - node_prob_in) <0.05:
+                    delta_exit += ratio_ou * (10)
+                    delta_exit += ratio_in * (10)
+                else:
+                    delta_exit -= ratio_ou * (in_clusters_weights[cluster_node] - node_prob_in)
+                    delta_exit -= ratio_in * (ou_clusters_weights[cluster_node] - node_prob_ou)
+                    
+                                                   
+            
                 delta_best = 0
                 cluster_best = cluster_node
 
                 for cluster in unique_clusters:
                     delta = 2 * neighbor_clusters_weights[cluster]
-                    delta -= ratio_ou * ( pow(in_clusters_weights[cluster],3))- 2 * ratio_ou * pow((1-in_clusters_weights[cluster]),3)
-                    delta -= ratio_in * ( pow(ou_clusters_weights[cluster],3))- 2 * ratio_in * pow((1-ou_clusters_weights[cluster]),3)
+                    if (in_clusters_weights[cluster]) >0.15:
+                        delta -= ratio_ou * (10)
+                        delta -= ratio_in * (10)
+                    
+                    elif (in_clusters_weights[cluster]) <0.05:
+                        delta += ratio_ou * (10)
+                        delta += ratio_in * (10)
+                    else:
+                        delta -= ratio_ou * in_clusters_weights[cluster]
+                        delta -= ratio_in * ou_clusters_weights[cluster]
 
                     delta_local = delta - delta_exit
                     if delta_local > delta_best:
